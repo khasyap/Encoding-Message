4083f14de4305898e536627c31b11434b92cbecf
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2025-10-14 23:22:22 +0530

    Update EncodingMessage.java

--- EncodingMessage.java
+++ EncodingMessage.java
@@ -1,1 +1,20 @@
-System.out.Println
+import java.io.*;
+import java.util.*;
+class UserMainCode
+  {
+    public String encodeMessage(String input1, int input2){
+      int sum=0;
+      for(char c:input1.toCharArray()){
+        sum+=(int)c;
+      }
+      int endIndex=input1.lenght();
+      while(sum>input2 && endIndex>0){
+        endIndex--;
+        sum-=(int) input1.charAt(endIndex);
+      }
+      if(sum>input2){
+        return "";
+      }
+      return input1.substring(0,endIndex);
+    }
+  }
